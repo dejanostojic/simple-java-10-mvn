87cabc10ef8a40fad57f103bb94a51ce17d1fc37
Dejan.Ostojic <ostdejan@gmail.com>
2018-07-08 13:33:11 +0200

    Add new new option to upper case for enpoint echo

--- src/main/java/com/example/demo/rest/RestController.java
+++ src/main/java/com/example/demo/rest/RestController.java
@@ -9,8 +9,18 @@ import org.springframework.web.bind.annotation.RequestParam;
 public class RestController {
 
     @RequestMapping(method = RequestMethod.GET, path = "/echo")
-    public String echoParam(@RequestParam String val){
+    public String echoParam(@RequestParam String val,
+                            @RequestParam(value="toUpper", required = false, defaultValue = "false")
+                                    boolean toUpper){
+        if (val == null){
+            throw new RuntimeException("Bad request. Param val is required");
+        }
+        if (toUpper){
+            return val.toUpperCase();
+        }
+
         return val;
     }
+
 
 }
