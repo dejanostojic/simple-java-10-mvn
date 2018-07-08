2584a34fa472264b07fad017f09eacb3c7456da1
Dejan.Ostojic <ostdejan@gmail.com>
2018-07-08 09:45:30 +0200

    Add rest api test

--- src/test/java/com/example/demo/rest/RestControllerTest.java
+++ src/test/java/com/example/demo/rest/RestControllerTest.java
@@ -1,4 +1,47 @@
 package com.example.demo.rest;
 
+import com.github.springtestdbunit.DbUnitTestExecutionListener;
+import com.github.springtestdbunit.annotation.DatabaseOperation;
+import com.github.springtestdbunit.annotation.DatabaseTearDown;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.HttpEntity;
+import org.springframework.test.context.TestExecutionListeners;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.support.DependencyInjectionTestExecutionListener;
+import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
+import org.springframework.test.context.transaction.TransactionalTestExecutionListener;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@TestExecutionListeners({ DependencyInjectionTestExecutionListener.class, TransactionalTestExecutionListener.class, DbUnitTestExecutionListener.class })
+//@DatabaseTearDown(type= DatabaseOperation.DELETE_ALL, value = {"classpath:all-tables.xml"})
 public class RestControllerTest {
+
+
+    @LocalServerPort
+    private int port;
+
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+
+    private String createURLWithPort(String uri) {
+        return "http://localhost:" + port + uri;
+    }
+
+
+    @Test
+    public void test(){
+        String body = restTemplate.getForEntity(createURLWithPort("/testJava10/echo") + "?val=echoVal", String.class)
+                .getBody();
+        assertThat(body).isEqualTo("echoVal");
+    }
 }
